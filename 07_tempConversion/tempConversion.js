1ca1157c23dbde3de1f78678f2b73e9eef0981bf
RaccoonishDeveloper <139078880+RaccoonishDeveloper@users.noreply.github.com.>
2023-11-06 13:17:38 +0200

    finished exercise 7.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,21 @@
-const convertToCelsius = function() {
+function convertToCelsius(fahrenheit) {
+  let converted = (fahrenheit - 32)/1.8;
+  return parseFloat(converted.toFixed(1))
 };
 
-const convertToFahrenheit = function() {
+function convertToFahrenheit(celsius) {
+  let converted = (celsius * 1.8)+ 32;
+  return parseFloat(converted.toFixed(1))
 };
+
+// MOST OPTIMAL
+// const convertToCelsius = function (fahrenheit) {
+//   return Math.round((fahrenheit - 32) * (5 / 9) * 10) / 10;
+// };
+
+// const convertToFahrenheit = function (celsius) {
+//   return Math.round(((celsius * 9) / 5 + 32) * 10) / 10;
+// };
 
 // Do not edit below this line
 module.exports = {
