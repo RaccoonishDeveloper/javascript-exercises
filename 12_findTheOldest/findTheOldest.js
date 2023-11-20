bc8a945ba77fe1d64db3b3537b7b57f4ba3793a3
RaccoonishDeveloper <139078880+RaccoonishDeveloper@users.noreply.github.com.>
2023-11-20 13:15:16 +0200

    finished final exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (array) {
+  const currentDate = new Date().getFullYear();
+  return array.reduce((oldest, currentPerson) => {
+    const oldAge = oldest.yearOfDeath
+      ? oldest.yearOfDeath - oldest.yearOfBirth
+      : currentDate - oldest.yearOfBirth;
+    const currentAge = currentPerson.yearOfDeath
+      ? currentPerson.yearOfDeath - currentPerson.yearOfBirth
+      : currentDate - currentPerson.yearOfBirth;
+    return oldAge < currentAge ? currentPerson : oldest;
+  });
 };
-
 // Do not edit below this line
 module.exports = findTheOldest;
